ace82f212b0d759fc8f8145c9aff9db9f56f4d93
Shannon Rayes <shannono18@gmail.com>
2021-10-12 11:19:22 -0400

    deleted json package and re-initiated due to errors

--- db/notes.js
+++
@@ -1,7 +0,0 @@
-let notes = [{
-    title: "Test Title",
-    text: "Test text",
-    id: 1
-  }]
-  
-  module.exports = notes
--- routes/index.js
+++
@@ -1,5 +0,0 @@
-const router = require('express').Router()
-
-router.use('/api', require('./noteRoutes.js'))
-
-module.exports = router
